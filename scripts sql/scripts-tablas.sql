a4338c418b224f0da4c9d6b32329005c6fd724c0
Iván Dueñas <ivan.duenas.mt@correo.usa.edu.co>
2021-09-30 12:47:37 -0500

    Creación tablas mensaje y cliente

---
+++ scripts sql/scripts-tablas.sql
@@ -0,0 +1,11 @@
+Create table CLIENT(
+    ID number PRIMARY KEY,
+    Name VARCHAR(4000),
+    Email VARCHAR2(20),
+    Age NUMBER
+);
+
+Create table MESSAGE(
+    ID NUMBER PRIMARY KEY,
+    Messagetext VARCHAR2(4000) 
+);
