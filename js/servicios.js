b154b1a1da04fcf771d2d010489242058a67e151
JuanRojasC <appsjuanrojas@gmail.com>
2021-09-30 20:46:18 -0500

    Socializacion Formulario de Mensajes

--- js/servicios.js
+++ js/servicios.js
@@ -1,5 +1,5 @@
 // Añadir en esta variable la url de su base de datos
-const baseUrl = ''
+const baseUrl = ""
 
 function listarMensajes() {
   $.ajax({
@@ -16,6 +16,7 @@ function listarMensajes() {
           <tr>
             <td>${messagetext}</td>
             <td><button onclick="eliminarMensaje(${id})">Eliminar</button></td>
+            <td><button onclick="leerMensaje(${id})">Leer</button></td>
           </tr>
       `
       })
@@ -58,3 +59,37 @@ function eliminarMensaje(id) {
 }
 
 listarMensajes()
+
+
+function leerMensaje(id){
+  $.ajax({
+    url: `${baseUrl}/ords/admin/message/message/${id}`,
+    type: 'GET',
+    datatype: 'JSON',
+    success: respuesta => {
+      let mensaje = respuesta.items[0];
+      console.log(mensaje)
+      $("#message-detail").html(`
+          <h2>Mensaje</h2>
+          <button class="boton-cerrar" id="boton-cerrar" onclick="cerrarDetalle()" >X</button>
+          <div>
+            <h3>Id: </h3>
+            <p class="id-text">${mensaje.id}</p>
+          </div>
+          <div>
+            <h3>Contenido: </h3>
+            <p class="message-text">${mensaje.messagetext}</p>
+          </div>
+      `)
+    },
+    error: e => {
+        console.log(e)
+    }   
+  })
+}
+
+function cerrarDetalle(){
+  $("#boton-cerrar").click(function(){
+    $("#message-detail").empty()
+  })
+}
